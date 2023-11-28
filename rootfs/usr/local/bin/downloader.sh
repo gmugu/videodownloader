3cd54636f6bebc2781bc9ee3bfd7b347854fc463
MuGu <94156510@qq.com>
2023-11-28 20:00:54 +0800

    fix: 视频文件名称为tmp时冲突

--- rootfs/usr/local/bin/downloader.sh
+++ rootfs/usr/local/bin/downloader.sh
@@ -10,13 +10,13 @@ name=$4
 if [ "$type" = "m3u8" ] || [ "$type" = "m3u8" ]; then
 
 # N_m3u8DL-RE输出的文件名为tmp.mp4
-echo "------开始使用 N_m3u8DL-RE 下载: N_m3u8DL-RE $url --tmp-dir $tmp_dir --save-dir $tmp_dir --save-name "tmp" --check-segments-count=false --auto-select"
+echo "------开始使用 N_m3u8DL-RE 下载: N_m3u8DL-RE $url --tmp-dir $tmp_dir --save-dir $tmp_dir --save-name "tmp_$name" --check-segments-count=false --auto-select"
 
-N_m3u8DL-RE $url --tmp-dir $tmp_dir --save-dir $tmp_dir --save-name "tmp" --check-segments-count=false --auto-select
+N_m3u8DL-RE $url --tmp-dir $tmp_dir --save-dir $tmp_dir --save-name "tmp_$name" --check-segments-count=false --auto-select
 
-echo "------下载完成, 开始转码: ffmpeg -i $tmp_dir/tmp.mp4 -c copy $tmp_dir/$name"
+echo "------下载完成, 开始转码: ffmpeg -i $tmp_dir/tmp_$name.mp4 -c copy $tmp_dir/$name"
 
-ffmpeg -hide_banner -i "$tmp_dir/tmp.mp4" -c copy "$tmp_dir/$name"
+ffmpeg -hide_banner -i "$tmp_dir/tmp_$name.mp4" -c copy "$tmp_dir/$name"
 
 else
 
